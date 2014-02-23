09d2ba1e7b9575d3241f8f089ff1ad8d589642bc
Derek Ludwig <derek.s.ludwig@gmail.com>
2014-02-23 15:22:38 -0800

    Switch to dzen/conky over xmobar

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -8,7 +8,6 @@ Config { font = "xft:Inconsolata-12:antialias=true"
        , persistent = False
        , hideOnStart = False
        , commands = [ Run Weather "CYVR" ["-t","<station>: <tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
-                    , Run Wireless "wlp2s0" ["-L","30","-H","70","--low","red", "--normal", "lightblue" ,"--high","green"] 10
                     , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Battery [] 10
@@ -17,5 +16,5 @@ Config { font = "xft:Inconsolata-12:antialias=true"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% } %cpu%  %memory%  %battery%  Wifi: %wlp2s0wi% { <fc=#ee9a00>%date%</fc> | %CYVR%"
+       , template = "%StdinReader% } %cpu%  %memory%  %battery% { <fc=#ee9a00>%date%</fc> | %CYVR%"
        }
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,37 +1,110 @@
 import XMonad
 import XMonad.Util.Run
 import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.WorkspaceCompare
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Accordion
 import XMonad.Layout.NoBorders -- (noBorders)
+import Data.List
+
+-- Solarized colors
+base03      = "#002b36"
+base02      = "#073642"
+base01      = "#586e75"
+base00      = "#657b83"
+base0       = "#839496"
+base1       = "#93a1a1"
+base2       = "#eee8d5"
+base3       = "#fdf6e3"
+yellow      = "#b58900"
+orange      = "#cb4b16"
+red         = "#dc322f"
+magenta     = "#d33682"
+violet      = "#6c71c4"
+blue        = "#268bd2"
+cyan        = "#2aa198"
+
+-- Default dark bg/fg
+background = base03
+foreground = base0
+bitmapsDir = ".xmonad/dzen2"
 
 myLayoutHook = smartBorders $ layoutHook defaultConfig
 myManageHook = composeAll (
-  [ className =? "Unity-2d-panel" --> doIgnore
-  , className =? "Unity-2d-launcher" --> doFloat
-  , className =? "xmbc.bin" --> doFullFloat
+  [ className =? "xmbc.bin" --> doFullFloat
   , isFullscreen --> doFullFloat
   ])
 
+quote color = "'" ++ color ++ "'"
+fontFlag = "-fn Inconsolata-12"
+dzenCmd = "/usr/bin/dzen2 " ++ flags
+  where
+    disableRightClick = "-e " ++ quote "button2=;"
+    screen = "-xs 1"
+    bg = "-bg " ++ quote background
+    fg = "-fg " ++ quote foreground
+    flags = intercalate " " [disableRightClick, screen, bg, fg, fontFlag, "-dock"]
+
+conkyCmd = "/usr/bin/conky | /usr/bin/dzen2 " ++ flags
+  where
+    disableRightClick = "-e " ++ quote "button2=;"
+    screen = "-xs 2"
+    bg = "-bg " ++ quote background
+    fg = "-fg " ++ quote foreground
+    flags = intercalate " " [disableRightClick, screen, bg, fg, fontFlag]
+
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
-  xmonad $ defaultConfig
+  -- xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
+  dzenproc <- spawnPipe dzenCmd
+  conkyproc <- spawnPipe conkyCmd
+  xmonad $ ewmh $ defaultConfig
               { modMask = mod4Mask
               , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
               , layoutHook = avoidStruts myLayoutHook
-              , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "blue" "" . shorten 80
-                        }
+              , logHook = dzenLogHook dzenproc
+              , normalBorderColor = base0
+              , focusedBorderColor = red
               } `additionalKeysP` theseKeys
+
+xmobarLogHook h = dynamicLogWithPP xmobarPP
+  { ppOutput = hPutStrLn h
+  , ppTitle = xmobarColor "blue" "" . shorten 80
+  , ppSort = getSortByXineramaPhysicalRule
+  , ppVisible = wrap "[" "]"
+  }
+
+dzenLogHook h = dynamicLogWithPP dzenPP
+  { ppOutput = hPutStrLn h
+  , ppTitle = dzenColor base0 base03 . shorten 100 . pad
+  , ppSort = getSortByXineramaPhysicalRule
+  , ppCurrent = dzenColor base0 base3 . pad
+  , ppVisible = dzenColor base1 base2 . pad
+  , ppHidden = dzenColor base00 base02 . pad
+  --, ppLayout = dzenColor base00 base02 . pad
+  , ppLayout = pad . (\x -> case x of
+      "Tall" -> "^i(" ++ bitmapsDir ++ "/tall.xbm)"
+      "Mirror Tall" -> "^i(" ++ bitmapsDir ++ "/mtall.xbm)"
+      "Full" -> "^i(" ++ bitmapsDir ++ "/full.xbm)"
+      _      -> x
+    )
+  }
+
+dmenuCmd = "/usr/bin/dmenu_run " ++ flags
+  where
+    bg = "-nb " ++ quote background
+    fg = "-nf " ++ quote foreground
+    sbg = "-sb " ++ quote base3
+    sfg = "-sf " ++ quote base0
+    flags = intercalate " " [fontFlag, fg, bg, sfg, sbg]
 
 theseKeys =
   [
     ("M4-<Escape>", spawn "xscreensaver-command -lock"),
-    ("M4-p", spawn "dmenu_run -fn Inconsolata-12"),
+    ("M4-p", spawn dmenuCmd),
     ("<XF86AudioRaiseVolume>", spawn "amixer set Master 1+"),
     ("<XF86AudioLowerVolume>", spawn "amixer set Master 1-")
   ]
